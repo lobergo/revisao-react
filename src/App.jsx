e9ffa9bea206bab38bfd9adbda55c915652e1d6d
lobergo <lorenabergo23@gmail.com>
2023-08-14 17:45:05 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Login</h1>
+      </header>
+
+      <main>
+        <form action="">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' placeholder='Digite seu Email' />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="senha">Senha</label>
+            <input type="password" id='senha' placeholder='Digite sua senha' />
+          </div>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default App
